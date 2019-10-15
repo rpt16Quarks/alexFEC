8d9d27a65e73435ce66838f6fdbfa2b02a2e9929
Alex Wallis <aldwallis@gmail.com>
2019-10-14 18:28:02 -0700

    Added testing from other branch

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,77 +2,139 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
-class App extends React.Component {
+export class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      items: []
+      firstPage: true,
+      item1: [],
+      item2: []
     };
     this.veryFetching = this.veryFetching.bind(this);
+    this.switchUp = this.switchUp.bind(this);
   }
 
   componentWillMount(){
     let prodId = window.location.search.split("=")[1];
-    if (prodId){
+    if (prodId == 1){
       console.log("prodId: ",prodId);
       fetch(`/suggested?prod_id=${prodId}`)
       .then(r => r.json())
       .then(d => {
+        let x = d.slice(0,5);
+        let y = d.slice(5,10);
         this.setState({
-          items: [d]
+          item1: [x],
+          item2: [y]
         })
       })
     } else {
       this.veryFetching();
     }
-
-
   }
 
   veryFetching(){
     fetch('/data')
     .then(r => r.json())
     .then(d => {
+      let x = d.slice(0,5);
+      let y = d.slice(5,10);
       this.setState({
-        items: d
+        item1: x,
+        item2: y
       })
+    })
+  }
+
+  switchUp(e){
+    //Essentially we want to go and click the button and it'll switch
+    //this.state.item1's value to this.state.item2's value
+    //Could do this by doing (prevState) or could just do {firstPage ? <allBS with item1> :<allBS with item2>}
+    this.setState((updater) => {
+      return {
+        firstPage: !updater.firstPage,
+        item1: updater.item2,
+        item2: updater.item1
+      }
     })
   }
 
 
 
-
   render(){
-    const ButtonR = styled.button`
+    const ButtonL = styled.button`
+      content: "";
       background-image: url(https://ir.ebaystatic.com/f/8ad5a0773b1335cc3ceb5d966215af2.svg);
-      background-position: -49px 6px
-      height: 70px;
-      width: 70px;
-      opacity: .4
-      // content: "";
-      // display: block
-      // margin-left: 6px;
-      // background-position: -55px 10px;
-      // transform: rotate(180deg)
+      display: block;
+      margin-left: 6px;
+      transform: rotate(180deg);
+      width: 15px;
+      height: 35px;
+      background-position: -49px 8px;
+      opacity: .4;
+      background-color: #fff;
+      border: 1px solid rgba(0,0,0,0.3);
+      border-radius: 35px;
+      position: absolute;
+      z-index: 1;
+      top: 24%;
     `
+    const ButtonR = styled.button`
+      content: "";
+      background-image: url(https://ir.ebaystatic.com/f/8ad5a0773b1335cc3ceb5d966215af2.svg);
+      display: flex;
+      //justify-content: flex-end;
+      //margin-left: 9px;
+      width: 15px;
+      height: 35px;
+      right: -2%;
+      background-position: -49px 8px;
+      opacity: .4;
+      background-color: #fff;
+      border: 1px solid rgba(0,0,0,0.3);
+      border-radius: 35px;
+      position: absolute;
+      z-index: 1;
+      top: 24%;
+      padding: 4px, 0;
+    `
+
+    const AllItems = styled.ul`
+      display: flex;
+      flex-direction: row;
+    `
+
+    const Photos = styled.img`
+      height: 175px;
+      width: 200px;
+    `
+    const Container = styled.div`
+
+    `
+
     return (
-      <div class="container">
-        Related Sponsored Content X/2
-        <ButtonR>X</ButtonR>
-        <ul class="allItems">
-            {this.state.items.map(item => {
+     <Container>
+        {this.state.firstPage ?
+          <p>Related Sponsored Content 1/2</p>
+          :<p>Related Sponsored Content 2/2</p>
+        }
+        <ButtonL onClick={this.switchUp}/>
+        <AllItems>
+            {this.state.item1.map(item => {
               return (
                 <li class="individualItems">
-                  <img src={item.image} alt="watch"/>
-                  {item.price}
-                  {item.productTitle}
-                  {item.shippingCost}
+                  <Photos src={item.image} alt="watch"/>
+                  <br></br>{item.productTitle}
+                  <br></br>${item.price}
+                  <br></br>{item.shippingCost == 0 ?
+                    'Free Shipping'
+                    :item.shippingCost
+                  }
                 </li>
-              )
-            })}
-          </ul>
-        <button class="buttonR"></button>
-      </div>
+                )})}
+          </AllItems>
+          <ButtonR onClick={this.switchUp}/>
+      </Container>
     )
   }
 }
